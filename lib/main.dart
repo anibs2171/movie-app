f7cf24d78f7e73eb62ff8c05b97236df8ed3c5cf
Narendiran Arthanarieswaran <narendiran.work@gmail.com>
2021-11-07 11:40:59 +0530

    Added box decoration to MovieCard

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:postgres/postgres.dart';
+import "movie_card.dart";
 
 void main() {
   runApp(MyApp());
@@ -32,33 +33,86 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Movie App'),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            getData();
-          },
-          child: Icon(Icons.add),
-        ),
-      ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+          List<Widget> children;
+      if (snapshot.hasData) {
+        return ListView.builder(itemBuilder: (context,index){
+          return MovieCard(
+            id: snapshot.data[index][0],
+            date: DateTime.fromMicrosecondsSinceEpoch(snapshot.data[index][2]),
+            movie_name: snapshot.data[index][1],
+            rating: snapshot.data[index][3].toString(),
+          );
+        },itemCount: snapshot.data.length,);
+        // <Widget>[
+        //   const Icon(
+        //     Icons.check_circle_outline,
+        //     color: Colors.green,
+        //     size: 60,
+        //   ),
+        //   Padding(
+        //     padding: const EdgeInsets.only(top: 16),
+        //     child: Text('Result: ${snapshot.data}'),
+        //   )
+        // ];
+      }
+      else if (snapshot.hasError) {
+        children = <Widget>[
+          const Icon(
+            Icons.error_outline,
+            color: Colors.red,
+            size: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text('Error: ${snapshot.error}'),
+          )
+        ];
+      }
+      else{
+        children = const <Widget>[
+          SizedBox(
+            child: CircularProgressIndicator(),
+            width: 60,
+            height: 60,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Text('Awaiting result...'),
+          )
+        ];
+      }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: children,
+            ),
+          );
+        },
+      )
     );
   }
 }
 
-class MovieCard extends StatelessWidget {
-  const MovieCard({Key? key}) : super(key: key);
+// class MovieCard extends StatelessWidget {
+//   const MovieCard({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text('Test');
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Text('Test');
-  }
-}
-
-void getData() async {
+Future getData() async {
   final connection = PostgreSQLConnection('ec2-52-200-155-213.compute-1.amazonaws.com', 5432, 'dbca0g0f9vhbak',
       username: 'btkydjfodbmlon', password: '9f0e0882442218b55ad591c941c7ce472eef22addf02a30df9413864d2d318d0',useSSL: true);
   await connection.open();
   var a = await connection.query('select * from movie');
-  print(a);
+  // print(a);
   print('works');
   await connection.close();
+  return a;
 }
--- lib/movie_card.dart
+++ lib/movie_card.dart
@@ -1,41 +1,29 @@
 import 'package:flutter/material.dart';
 
 class MovieCard extends StatelessWidget {
-  const MovieCard(
-      {this.movieName,
-      this.genre,
-      this.starring,
-      this.director,
-      this.image,
-      this.pageViews,
-      this.votedBy,
-      this.language,
-      this.voting,
-      this.runtime,
-      this.time});
-  final String? movieName;
-  final String? genre;
-  final List? starring;
-  final List? director;
-  final String? image;
-  final int? pageViews;
-  final int? votedBy;
-  final String? language;
-  final int? voting;
-  final int? runtime;
-  final int? time;
+ MovieCard({required this.id, required this.date, required this.movie_name,required this.rating,});
+  final int? id;
+  final String? movie_name;
+  // final String? genre;
+  final DateTime? date;
+  final String? rating;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 20),
-      child: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Color(0xFFF1C085),
+    return GestureDetector(
+      onTap: (){
+
+      },
+      child: Padding(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 20),
+        child: Container(
+          // color: Colors.grey,
+          width: double.infinity,
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.white70,
           borderRadius: BorderRadius.circular(10.0),
-          border: Border.all(color: Colors.white, width: 1.0),
-          boxShadow: [
+              boxShadow: [
             BoxShadow(
               color: Colors.black26,
               offset: const Offset(
@@ -52,137 +40,216 @@ class MovieCard extends StatelessWidget {
               spreadRadius: 0.0,
             ), //BoxShadow
           ],
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.arrow_drop_up_outlined,
-                        size: 30.0,
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        "$voting",
-                        style: TextStyle(fontSize: 25),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Icon(
-                        Icons.arrow_drop_down_outlined,
-                        size: 30.0,
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        "Votes",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    //height: 160,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25.0),
-                      // image: DecorationImage(
-                      //   fit: BoxFit.fill,
-                      //   image: NetworkImage("${image!}"),
-                      // ),
-                    ),
-                    child: Image.network(image!),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "$movieName",
-                          style: TextStyle(fontSize: 30),
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          "Genre: $genre",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                        SizedBox(
-                          height: 3.0,
-                        ),
-                        Text(
-                          "Director: ${director!.join(' , ')}",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                        SizedBox(
-                          height: 3.0,
-                        ),
-                        Text(
-                          "Starring: ${starring!.join(', ')}",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                        SizedBox(
-                          height: 3.0,
-                        ),
-                        Text(
-                          "$runtime Mins  |  $language  |  ${DateTime.fromMicrosecondsSinceEpoch(time!).day} ",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                        SizedBox(
-                          height: 3.0,
-                        ),
-                        Text(
-                          "$pageViews views | Voted by $votedBy people",
-                          style: TextStyle(fontSize: 15, color: Colors.blue),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              InkWell(
-                onTap: () {},
-                child: new Container(
-                  width: double.infinity,
-                  height: 50.0,
-                  decoration: new BoxDecoration(
-                    color: Colors.blue,
-                    border: new Border.all(color: Colors.white, width: 2.0),
-                    borderRadius: new BorderRadius.circular(15.0),
-                  ),
-                  child: new Center(
-                    child: new Text(
-                      'Watch Trailer',
-                      style: new TextStyle(
-                          fontSize: 25.0,
-                          color: Color(0xFFF1C085),
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              )
-            ],
           ),
-        ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.add_a_photo,size: 60,),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(id.toString()),
+                    Text(movie_name!),
+                    // Text(genre!),
+                    Text(date.toString()),
+                    Text(rating!),
+                  ],
+                ),
+              ],
+            ),
+          ),
+      ),
       ),
     );
   }
 }
+
+
+// class MovieCard extends StatelessWidget {
+//   const MovieCard(
+//       {this.movieName,
+//       this.genre,
+//       this.starring,
+//       this.director,
+//       this.image,
+//       this.pageViews,
+//       this.votedBy,
+//       this.language,
+//       this.voting,
+//       this.runtime,
+//       this.time});
+//   final String? movieName;
+//   final String? genre;
+//   final List? starring;
+//   final List? director;
+//   final String? image;
+//   final int? pageViews;
+//   final int? votedBy;
+//   final String? language;
+//   final int? voting;
+//   final int? runtime;
+//   final int? time;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 20),
+//       child: Container(
+//         width: double.infinity,
+//         decoration: BoxDecoration(
+//           color: Color(0xFFF1C085),
+//           borderRadius: BorderRadius.circular(10.0),
+//           border: Border.all(color: Colors.white, width: 1.0),
+//           boxShadow: [
+//             BoxShadow(
+//               color: Colors.black26,
+//               offset: const Offset(
+//                 5.0,
+//                 5.0,
+//               ),
+//               blurRadius: 10.0,
+//               spreadRadius: 2.0,
+//             ), //BoxShadow
+//             BoxShadow(
+//               color: Colors.white,
+//               offset: const Offset(0.0, 0.0),
+//               blurRadius: 0.0,
+//               spreadRadius: 0.0,
+//             ), //BoxShadow
+//           ],
+//         ),
+//         child: Padding(
+//           padding: EdgeInsets.all(10.0),
+//           child: Column(
+//             children: [
+//               Row(
+//                 children: [
+//                   Column(
+//                     children: [
+//                       Icon(
+//                         Icons.arrow_drop_up_outlined,
+//                         size: 30.0,
+//                       ),
+//                       SizedBox(
+//                         height: 10.0,
+//                       ),
+//                       Text(
+//                         "$voting",
+//                         style: TextStyle(fontSize: 25),
+//                       ),
+//                       SizedBox(
+//                         height: 10.0,
+//                       ),
+//                       Icon(
+//                         Icons.arrow_drop_down_outlined,
+//                         size: 30.0,
+//                       ),
+//                       SizedBox(
+//                         height: 10.0,
+//                       ),
+//                       Text(
+//                         "Votes",
+//                         style: TextStyle(fontSize: 16),
+//                       ),
+//                     ],
+//                   ),
+//                   SizedBox(
+//                     width: 20,
+//                   ),
+//                   Container(
+//                     //height: 160,
+//                     width: 100,
+//                     decoration: BoxDecoration(
+//                       borderRadius: BorderRadius.circular(25.0),
+//                       // image: DecorationImage(
+//                       //   fit: BoxFit.fill,
+//                       //   image: NetworkImage("${image!}"),
+//                       // ),
+//                     ),
+//                     child: Image.network(image!),
+//                   ),
+//                   SizedBox(
+//                     width: 20,
+//                   ),
+//                   Expanded(
+//                     child: Column(
+//                       mainAxisAlignment: MainAxisAlignment.start,
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text(
+//                           "$movieName",
+//                           style: TextStyle(fontSize: 30),
+//                         ),
+//                         SizedBox(
+//                           height: 5.0,
+//                         ),
+//                         Text(
+//                           "Genre: $genre",
+//                           style: TextStyle(fontSize: 15),
+//                         ),
+//                         SizedBox(
+//                           height: 3.0,
+//                         ),
+//                         Text(
+//                           "Director: ${director!.join(' , ')}",
+//                           style: TextStyle(fontSize: 15),
+//                         ),
+//                         SizedBox(
+//                           height: 3.0,
+//                         ),
+//                         Text(
+//                           "Starring: ${starring!.join(', ')}",
+//                           style: TextStyle(fontSize: 15),
+//                         ),
+//                         SizedBox(
+//                           height: 3.0,
+//                         ),
+//                         Text(
+//                           "$runtime Mins  |  $language  |  ${DateTime.fromMicrosecondsSinceEpoch(time!).day} ",
+//                           style: TextStyle(fontSize: 15),
+//                         ),
+//                         SizedBox(
+//                           height: 3.0,
+//                         ),
+//                         Text(
+//                           "$pageViews views | Voted by $votedBy people",
+//                           style: TextStyle(fontSize: 15, color: Colors.blue),
+//                         ),
+//                       ],
+//                     ),
+//                   )
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: 5.0,
+//               ),
+//               InkWell(
+//                 onTap: () {},
+//                 child: new Container(
+//                   width: double.infinity,
+//                   height: 50.0,
+//                   decoration: new BoxDecoration(
+//                     color: Colors.blue,
+//                     border: new Border.all(color: Colors.white, width: 2.0),
+//                     borderRadius: new BorderRadius.circular(15.0),
+//                   ),
+//                   child: new Center(
+//                     child: new Text(
+//                       'Watch Trailer',
+//                       style: new TextStyle(
+//                           fontSize: 25.0,
+//                           color: Color(0xFFF1C085),
+//                           fontWeight: FontWeight.bold),
+//                     ),
+//                   ),
+//                 ),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
